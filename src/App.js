d9129ae4e8a207a91d5a4ec7a57538774ffa6f14
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-09 07:41:34 +0300

    Updated App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import uuid from "uuid"
+import React, { useState } from "react";
+import uuid from "uuid";
 
-import TodoList from './components/TodoList';
-import TodoInput from './components/TodoInput';
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
 
 function App() {
+  const [values, setValues] = useState({
+    items: [
+      { id: 1, title: "Title 1" },
+      { id: 2, title: "Title 2" },
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  });
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
+  const clearList = (e) => {};
+
+  const handleDelete = (e) => {};
+
+  const handleEdit = (e) => {};
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <TodoInput />
-          <TodoList />
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-5">
+          <h3 className="text-center text-capitalize">Todo input</h3>
+
+          <TodoInput
+            item={values.item}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            editItem={values.editItem}
+          />
+
+          <TodoList
+            items={values.items}
+            clearList={clearList}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
         </div>
       </div>
     </div>
