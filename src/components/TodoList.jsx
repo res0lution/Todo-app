cc73dd1f9bed6eac0433d89af44029dc1a8d3bf1
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-09 08:25:12 +0300

    Updated TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,29 @@ import React from "react";
 
 import TodoItem from "./TodoItem";
 
-function TodoList() {
+function TodoList({ items, clearList, handleDelete, handleEdit }) {
   return (
-    <section className="App">
-      <h2>Todo List</h2>
-      <TodoItem />
+    <section>
+      <ul className="list group my-5">
+        <h3 className="text-capitalize text-center">Todo List</h3>
+
+        {items.map((item) => (
+          <TodoItem
+            key={item.id}
+            title={item.title}
+            handleDelete={() => handleDelete(item.id)}
+            handleEdit={() => handleEdit(item.id)}
+          />
+        ))}
+      </ul>
+
+      <button
+        onClick={clearList}
+        type="button"
+        className="btn btn-danger text-uppercase mt-5"
+      >
+        Cleat List
+      </button>
     </section>
   );
 }
