e190c06cfb2c6f8551e6b108d90f72afa38085e9
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-09 06:46:25 +0300

    Added Bootstrap and created file structure

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoInput() {
+  return (
+    <div className="App">
+      <h1>TodoInput</h1>
+    </div>
+  );
+}
+
+export default TodoInput;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+  return (
+    <section className="App">
+      <h2>Todo List</h2>
+      <TodoItem />
+    </section>
+  );
+}
+
+export default TodoList;
