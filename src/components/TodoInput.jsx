18644c342f39cb86daaec49927233caa268a5297
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-09 07:58:24 +0300

    Updated Input component

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-function TodoInput() {
+function TodoInput({ item, handleChange, handleSubmit, editItem }) {
   return (
-    <div className="App">
-      <h1>TodoInput</h1>
+    <div className="card card-body my-3">
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <div className="input-group-text bg-primary text-white">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </div>
+          </div>
+
+          <input
+            type="text"
+            onChange={handleChange}
+            value={item}
+            className="form-control text-capitalize"
+            placeholder="Add todo item"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block btn-primary mt-3 text-uppercase">
+          Add item
+        </button>
+      </form>
     </div>
   );
 }
